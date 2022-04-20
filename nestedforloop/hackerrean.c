f75e72a5c0a5b8110a18fabf17e3f137c017e30e
ayushsachan123 <ayushsachan.in@gmail.com>
2022-04-20 20:21:50 +0530

    initial commit

---
+++ nestedforloop/hackerrean.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,m;
+    float a,b;
+    printf("enter two no.");
+	scanf("%d",&n);
+    scanf("%d",&m);
+    printf("%d %d",n+m, n-m);
+    scanf("%f%f",&a,&b);
+    printf("%f%f",a+b,a-b);
+    
+    return 0;
+}
