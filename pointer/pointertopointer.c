31fad287e637e25163572af9e51608a451e4ed98
ayushsachan123 <ayushsachan.in@gmail.com>
2022-04-21 19:25:33 +0530

    initial commit

--- pointer/pointertopointer.c
+++ pointer/pointertopointer.c
@@ -4,7 +4,7 @@ int main()
     float price= 100.00;
     float *ptr = &price;
     float **pptr = &ptr;
-    printf("adderss of *pptr is %f\n ",*ptr);
-       printf("adderss of **pptr is %f ",**pptr);
+    printf("value of *pptr is %f\n ",*ptr);
+       printf("value of **pptr is %f ",**pptr);
     return 0;
 }
